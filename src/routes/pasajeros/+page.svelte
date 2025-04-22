7655075121465d86fb6234776ab77b99aa3603b2
ayos <ayos007@gmail.com>
2025-04-21 21:57:20 -0400

    modify

--- src/routes/pasajeros/+page.svelte
+++ src/routes/pasajeros/+page.svelte
@@ -1,22 +1,17 @@
 <script>
-	import Carreras from "../../components/carreras.svelte";
-	import Pasajeromanual from "../../components/pasajeromanual.svelte";
-    
-
-    
+	import Carreras from '../../components/carreras.svelte';
+	import Pasajeromanual from '../../components/pasajeromanual.svelte';
 </script>
 
-<button class="refresh-button" on:click={() => window.location.reload()}>
-	<svg class="refresh-icon" viewBox="0 0 24 24">
-	  <path fill="currentColor" d="M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z"/>
-	</svg>
-	<span class="">Actualizar</span>
-</button>
-
-<div>
-<Pasajeromanual />
-</div>
-<div>
-<Carreras /> 
+<div class="d-flex justify-content-center m-4">
+	<button class="btn btn-primary" on:click={() => window.location.reload()}>
+		<span class="">Actualizar para ver pasajeros de aplicación</span>
+	</button>
 </div>
 
+<div>
+	<Pasajeromanual />
+</div>
+<div>
+	<Carreras />
+</div>
